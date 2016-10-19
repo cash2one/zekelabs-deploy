a47740539231f21bf16a2f8eb2f6958fc619c2db
root <curanest@gmail.com>
2016-10-19 16:35:54 +0000

    working

--- zekelabs/local_settings.py
+++ zekelabs/local_settings.py
@@ -5,11 +5,12 @@
 # have to be manually restarted because changes will not be noticed
 # immediately.
 
-DEBUG = True
+DEBUG = False 
 
 # Make these unique, and don't share it with anybody.
 SECRET_KEY = "ts7)f9-#i1&qozc@@a5u(7qhnx%vu@q3k9uj$sbxe=rdq4eyg3"
 NEVERCACHE_KEY = "*401$k#t66q)4_l6&0h+fmv9aa19$9(pp^%5=(b$(vt$h$1q7#"
+
 
 DATABASES = {
     "default": {
@@ -29,25 +30,26 @@ DATABASES = {
     }
 }
 
+
 ###################
 # DEPLOY SETTINGS #
 ###################
 
 # Domains for public site
-# ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["www.zekelabs.com"]
 
 # These settings are used by the default fabfile.py provided.
 # Check fabfile.py for defaults.
 
-# FABRIC = {
-#     "DEPLOY_TOOL": "rsync",  # Deploy with "git", "hg", or "rsync"
-#     "SSH_USER": "",  # VPS SSH username
-#     "HOSTS": [""],  # The IP address of your VPS
-#     "DOMAINS": ALLOWED_HOSTS,  # Edit domains in ALLOWED_HOSTS
-#     "REQUIREMENTS_PATH": "requirements.txt",  # Project's pip requirements
-#     "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
-#     "DB_PASS": "",  # Live database password
-#     "ADMIN_PASS": "",  # Live admin user password
-#     "SECRET_KEY": SECRET_KEY,
-#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
-# }
+FABRIC = {
+    "DEPLOY_TOOL": "rsync",  # Deploy with "git", "hg", or "rsync"
+    "SSH_USER": "curanest",  # VPS SSH username
+    "HOSTS": ["139.59.8.27"],  # The IP address of your VPS
+    "DOMAINS": ALLOWED_HOSTS,  # Edit domains in ALLOWED_HOSTS
+    "REQUIREMENTS_PATH": "requirements.txt",  # Project's pip requirements
+    "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
+    "DB_PASS": "root123",  # Live database password
+    "ADMIN_PASS": "code4curanest",  # Live admin user password
+    "SECRET_KEY": SECRET_KEY,
+    "NEVERCACHE_KEY": NEVERCACHE_KEY,
+}
